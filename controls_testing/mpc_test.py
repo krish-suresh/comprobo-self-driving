c3a879bcc4cc6691b08d508f5b57fa04d7587c0b
Krishna Suresh <krishna4132@gmail.com>
2022-11-12 02:18:36 -0500

    linearization and lqr progress

--- controls_testing/mpc_test.py
+++ controls_testing/mpc_test.py
@@ -2,29 +2,49 @@ from matplotlib.patches import FancyArrow
 import numpy as np
 import time
 import matplotlib.pyplot as plt
+import control
 ## Tasks
 # [X] Non linear dynamics
 # [X] Visualize control inputs
-# [ ] Linearization function
-# [ ] Use LQR to find control mat
-# [ ] 
-
+# [X] Linearization function
+# [X] Use LQR to find control mat
+# [ ] Draw wheels and wheel base and target poses
+# [ ] Add target pose and test lqr
+# [ ] Implement MPC controls 
 ## Notes
 # motor accel to speed?
 L = 0.2
 t_delta = 0.1 # sec
-x = [0,0,0] # x, y, theta
-u = [0.3,0.8] #steer, speed
-
+x = [0,0,0,0.4,1] # x, y, theta, steer_angle, forward_speed
+u = [0,0] # steer_angle_speed, forward_accel
+B = np.array([[0,0],
+              [0,0],
+              [0,0],
+              [1,0],  # steering angle
+              [0,1]]) # forward accel
+Q = np.eye(5)
+R = np.eye(2)
 def non_linear_dynamics(x, u):
-    heading = x[2]
-    steer = u[0]
-    speed = u[1]
     x_dot = np.zeros_like(x)
-    x_dot[0] = speed*np.cos(heading) # x_dot
-    x_dot[1] = speed*np.sin(heading) # y_dot
-    x_dot[2] = np.tan(steer)*speed/L # theta_dot
+    x_dot[0] = x[4]*np.cos(x[2]) # x_dot
+    x_dot[1] = x[4]*np.sin(x[2]) # y_dot
+    x_dot[2] = np.tan(x[3])*x[4]/L # theta_dot
+    x_dot[3] = u[0] # steer_angular_speed
+    x_dot[4] = u[1] # forward_accel 
     return x_dot
+
+def linearization(x): 
+    A = np.array([[0, 0, -np.sin(x[2])*x[4], 0, np.cos(x[2])],
+                  [0, 0, np.cos(x[2])*x[4], 0, np.sin(x[2])],
+                  [0, 0, 0, (1/(np.cos(x[3])**2))*x[4]/L ,np.tan(x[3])/L],
+                  [0, 0, 0, 0, 0],
+                  [0, 0, 0, 0, 0]])
+    return A
+A = linearization(x)
+K = control.lqr(A,B,Q,R)[0]
+print(np.linalg.eig(A-np.matmul(B,K))[0])
+print(K)
+quit()
 
 plt.ion()
 W = 2
@@ -36,6 +56,7 @@ ax.set_ylim([-W, W])
 arrow : FancyArrow = ax.arrow(x[0], x[1], 0.5, 0.5, head_width=0.05, head_length=0.1, fc='k', ec='k')
 
 while True:
+
     x_dot = non_linear_dynamics(x, u)
     x += x_dot*t_delta
     # print(x)
