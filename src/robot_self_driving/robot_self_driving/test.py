f8215cf282654193d1ee6dd3a95eb7ba0a44c288
ayushchakra <achakraborty@olin.edu>
2022-12-08 02:06:23 -0500

    changed encoder libraries and created odom node

--- src/robot_self_driving/robot_self_driving/test.py
+++
@@ -1,7 +0,0 @@
-import importlib
-encoder = importlib.import_module('rpi-rotary-encoder-python.encoder')
-
-def valueChanged(value):
-    print(value)
-
-e1 = encoder.Encoder(26, 19, callback=valueChanged)
