b59a6dbfdfffdae9f176643a34bc3bb4c36cc685
Krishna Suresh <krishna4132@gmail.com>
2022-12-08 07:45:52 +0000

    added odom package

--- src/robot_self_driving/robot_self_driving/encoder.py
+++ src/robot_self_driving/robot_self_driving/encoder.py
@@ -16,7 +16,7 @@ class OdometryNode(Node):
         self.imu_sub = self.create_subscription(Float64, "/imu_yaw", self.process_imu, 10)
         self.curr_heading = None
 
-        self.encoder = Encoder(ENCODER_GPIO_PIN_ONE, ENCODER_GPIO_PIN_TWO)
+        self.encoder = Encoder.Encoder(ENCODER_GPIO_PIN_ONE, ENCODER_GPIO_PIN_TWO)
         self.curr_ticks = self.encoder.read()
         self.prev_ticks = self.curr_ticks
 
@@ -33,9 +33,17 @@ class OdometryNode(Node):
         delta_ticks = self.curr_ticks - self.prev_ticks
         self.prev_ticks = self.curr_ticks
         dist_travelled = delta_ticks/TOTAL_ENCODER_TICKS*(2*np.pi*WHEEL_RADIUS)
-        self.pos_x += dist_travelled * np.cos(self.curr_heading)
-        self.pos_y += dist_travelled * np.sin(self.curr_heading)
+        print(self.curr_heading)
+        if self.curr_heading is not None:
+            self.pos_x += dist_travelled * np.cos(self.curr_heading)
+            self.pos_y += dist_travelled * np.sin(self.curr_heading)
         print(f"X: {self.pos_x}, Y: {self.pos_y}")
 
     def process_imu(self, msg: Float64):
-        self.curr_heading = msg.data
+        self.curr_heading = np.deg2rad(msg.data)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = OdometryNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
