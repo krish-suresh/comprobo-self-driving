7c2ccab2102d4e611c4ebe8692dcf31f73b7b9cf
Krishna Suresh <krishna4132@gmail.com>
2022-12-13 05:07:11 -0500

    steering clip

--- src/robot_self_driving/robot_self_driving/drive.py
+++ src/robot_self_driving/robot_self_driving/drive.py
@@ -44,7 +44,7 @@ class AckermannDrive():
         self.TURN_PHI_MAX = np.radians(32)
         self.TURN_PHI_MIN = np.radians(-47)
         self.MAX_VEL: float = ((self.MAX_RAW_RPM / self.GEAR_RATIO)/60) * ((self.TIRE_DIAMETER * np.pi))  # m/s
-        self.state = np.array([0, 0, -np.pi/2, 0, 0.01])  # x, y, theta, steer_angle, forward_speed
+        self.state = np.array([0, 0, np.pi/2, 0, 0.01])  # x, y, theta, steer_angle, forward_speed
         self.start_heading = self.state[2]
         self.steering_angle = 0
         self.u = np.zeros((2,1))
@@ -207,6 +207,7 @@ class AckermannDrive():
         current_time = time.time_ns()
         t_delta: float = (current_time - self.previous_set_input_time)/ (10 ** 9)
         self.state[3:] += u*t_delta
+        self.state[3] = np.clip(self.state[3], np.radians(-37), np.radians(37))
         self.set_steering_angle(self.state[3])
         self.set_drive_velocity(self.state[4])
         self.previous_set_input_time = current_time
