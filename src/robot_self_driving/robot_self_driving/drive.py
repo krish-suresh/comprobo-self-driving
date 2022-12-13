9e1ec8f3f56100098c8661b0d5903abeedf81c07
Krishna Suresh <krishna4132@gmail.com>
2022-12-13 09:49:57 +0000

    added spline following to track

--- src/robot_self_driving/robot_self_driving/drive.py
+++ src/robot_self_driving/robot_self_driving/drive.py
@@ -21,7 +21,6 @@ class AckermannDrive():
         self.connect = pigpio.pi()
         self.ros_node = ros_node
         self.imu_sub = self.ros_node.create_subscription(Imu, "/imu", self.process_imu, 10)
-        self.start_heading = None
         self.curr_heading = None
         self.orientation: Quaternion = None
         self.encoder_sub = self.ros_node.create_subscription(Int64MultiArray, "/encoder", self.process_encoder, 10)
@@ -45,7 +44,8 @@ class AckermannDrive():
         self.TURN_PHI_MAX = np.radians(32)
         self.TURN_PHI_MIN = np.radians(-47)
         self.MAX_VEL: float = ((self.MAX_RAW_RPM / self.GEAR_RATIO)/60) * ((self.TIRE_DIAMETER * np.pi))  # m/s
-        self.state = np.array([0, 0, 0, 0, 0.01])  # x, y, theta, steer_angle, forward_speed
+        self.state = np.array([0, 0, -np.pi/2, 0, 0.01])  # x, y, theta, steer_angle, forward_speed
+        self.start_heading = self.state[2]
         self.steering_angle = 0
         self.u = np.zeros((2,1))
         self.previous_odom_time = None
@@ -55,10 +55,10 @@ class AckermannDrive():
     def process_imu(self, msg: Imu):
         self.orientation = msg.orientation
         self.angular_vel = msg.angular_velocity
-        (_, _, yaw) = -euler_from_quaternion([self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w])
-        if not self.start_heading:
-            self.start_heading = np.deg2rad(yaw)
-        self.curr_heading = (np.deg2rad(yaw) - self.start_heading + np.pi) % (2 * np.pi) - np.pi
+        (_, _, yaw) = euler_from_quaternion([self.orientation.x, self.orientation.y, self.orientation.z, self.orientation.w])
+        # if not self.start_heading:
+        #     self.start_heading = yaw
+        self.curr_heading = (yaw - self.start_heading + np.pi) % (2 * np.pi) - np.pi
 
     def process_encoder(self, msg: Int64MultiArray):
         if len(self.start_encoder_ticks) == 0:
@@ -140,33 +140,34 @@ class AckermannDrive():
         if len(self.curr_encoder_ticks) == 0:
             return
         cur_time = time.time_ns()
+        # print(f'Ticks: {self.curr_encoder_ticks}')
+        # print(f'Heading: {self.curr_heading}')
         delta_ticks = self.curr_encoder_ticks - self.prev_encoder_ticks
         dist_travelled = delta_ticks/self.TOTAL_ENCODER_TICKS*(2*np.pi*self.WHEEL_ENCODER_RADIUS)
         self.state[0] += dist_travelled[0] * np.cos(self.curr_heading) + dist_travelled[1] * np.sin(self.curr_heading)
         self.state[1] += dist_travelled[0] * np.sin(self.curr_heading) + dist_travelled[1] * np.cos(self.curr_heading)
         self.state[2] = self.curr_heading
-        self.logger.info(str(self.curr_encoder_ticks))
+        # self.logger.info(str(self.state))
         # self.state[3] = self.steering_angle
         # self.state[4] = dist_travelled/(cur_time - self.previous_odom_time)*10**9 + 0.01
         self.previous_odom_time = cur_time
         self.prev_encoder_ticks = self.curr_encoder_ticks
-        print(self.time)
-        self.curr_odom = Odometry(
-            header=Header(),
-            child_frame_id='',
-            pose=PoseWithCovariance(
-                pose=Pose(
-                    position = Point(x=self.state[0], y=self.state[1])),
-                    orientation = self.orientation
-            ),
-            twist=TwistWithCovariance(
-                twist=Twist(
-                    linear=Vector3(x=self.state[4]*np.cos(self.curr_heading), y=self.state[4]*np.sin(self.curr_heading)),
-                    angular=self.angular_vel
-                )
-            )
-        )
-        self.odom_pub.publish(self.curr_odom)
+        # self.curr_odom = Odometry(
+        #     header=Header(),
+        #     child_frame_id='',
+        #     pose=PoseWithCovariance(
+        #         pose=Pose(
+        #             position = Point(x=self.state[0], y=self.state[1])),
+        #             orientation = self.orientation
+        #     ),
+        #     twist=TwistWithCovariance(
+        #         twist=Twist(
+        #             linear=Vector3(x=self.state[4]*np.cos(self.curr_heading), y=self.state[4]*np.sin(self.curr_heading)),
+        #             angular=self.angular_vel
+        #         )
+        #     )
+        # )
+        # self.odom_pub.publish(self.curr_odom)
 
     def non_linear_dynamics(self):
         x = self.get_state().to_vector()
--- src/robot_self_driving/robot_self_driving/spline_follow_test_node.py
+++ src/robot_self_driving/robot_self_driving/spline_follow_test_node.py
@@ -16,15 +16,16 @@ class SplineFollowTestNode(Node):
         self.timer = self.create_timer(timer_period, self.run_loop)
         self.robot = Robot(self, use_sim=False)
         # self.robot.drive.arm_esc() # TODO figure out way to detect if already on and only arm if power is off
-        # track = RaceTrack("/home/ubuntu/ros_ws/src/comprobo_self_driving/src/robot_self_driving/tracks/IMS_raceline.csv", 12.5, 7)
+        track = RaceTrack("/home/ubuntu/ros_ws/src/comprobo_self_driving/src/robot_self_driving/tracks/Monza_raceline.csv", 35, 9)
+        x, y = track.access_coords()
         # sp = track.create_spline()
-        x = [0, 1]
-        y = [0, 0]
+        # x = [0, 1]
+        # y = [0, 0]
         # x = [0, 1, 1+2**.5/2, 1+2**.5/2, 1, 0, -1, -1-2**.5/2, -1-2**.5/2, -1, 0]
         # y = [0, 0, 1-2**.5/2, 1+2**.5/2, 2, 2,  2,  1+2**.5/2,  1-2**.5/2,  0, 0]
         sp = CubicSpline2D(x,y)
         # mp = TrapezoidalMotionProfile(sp.s[-1],.2,1)        
-        mp = RotationLimitedMotionProfile(sp,0.4,0.5,0.5,0.01)
+        mp = RotationLimitedMotionProfile(sp,0.2,0.5,0.5,0.01)
         trajectory = CubicSplineTrajectory(sp, mp)
         self.robot.controller.follow_trajectory(trajectory)
 
